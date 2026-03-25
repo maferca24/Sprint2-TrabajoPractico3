67a9f7e7ff0e3a119ae0fb2a4930f8e8c2be132f
MAFERCARRE <mafercarrenio@gmail.com>
2026-03-25 16:49:28 -0300

    configuré rutas

---
+++ src/routes/superHeroesRoutes.mjs
@@ -0,0 +1,21 @@
+//Define las rutas necesarias para cada operación del controlador
+//La capa de ruta define los endpoints y mapea cada uno a su respectivo controlador
+//permitiendo que las solicitudes HTTP se manejen de forma estructurada y predecible
+
+
+import 'express';
+import {
+    obtenerSuperheroePorIdController,
+    obtenerTodosLosSuperheroesController,
+    buscarSuperheroesPorAtributoController,
+    obtenerSuperheroesMayoresDe30Controller
+} from '../controllers/superheroesControllers.mjs';
+
+const router =XPathExpression. Router();
+
+router.get('/heroes', obtenerTodosLosSuperheroesController);
+router.get('/heroes/:id', obtenerSuperheroePorIdController);
+router.get('/hero/buscar/:atributo/:valor', buscarSuperheroesPorAtributoController);
+router.get('/heroes/mayores-30', obtenerSuperheroesMayoresDe30Controller);
+
+export default router;
